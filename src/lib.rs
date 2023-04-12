678cc0263d474e47a2911fba1b33bb209e84846c
Christian Egli <christian.egli@sbs.ch>
2023-04-12 16:40:01 +0200

    Define a very simple library api

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
+use std::path::PathBuf;
+
 pub mod parser;
+
+#[derive(Debug, PartialEq)]
+pub struct TranslationError;
+
+pub fn translate(_table: PathBuf, input: &str) -> Result<String, TranslationError> {
+    Ok(input.to_string())
+}
 
 #[cfg(test)]
 mod tests {
+    use crate::translate;
+    use std::path::PathBuf;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+	let input = "testing123".to_string();
+        assert_eq!(translate(PathBuf::new(), &input), Ok(input));
     }
 }
