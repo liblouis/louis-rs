9249a51c4ece8ffd6ec330ca543f0befe8c2d639
Christian Egli <christian.egli@sbs.ch>
2023-04-13 11:18:29 +0200

    Added comments to improve the help message

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,20 @@ use std::path::PathBuf;
 
 #[derive(Debug, Subcommand)]
 enum Commands {
+    /// translate <INPUT> to or from braille using <TABLE>
     #[command(arg_required_else_help = true)]
     Translate {
+	/// Braille table to use for the translation
 	table: PathBuf,
+	/// String to translate
 	input: String,
     }
 }
 
 #[derive(Debug, Parser)] // requires `derive` feature
 #[command(name = "louis")]
-#[command(about = "A command line to translate to and from Braille", long_about = None)]
+#[command(about = "A command line tool to translate to and from Braille")]
+#[command(author, version, long_about = None)] // Read from `Cargo.toml`
 struct Cli {
     #[command(subcommand)]
     command: Commands,
