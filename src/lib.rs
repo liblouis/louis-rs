5cb8249d35bd6e6856f3f2848219424b9cdeb5b5
Christian Egli <christian@egli.cc>
2023-04-21 18:06:21 +0200

    Attempt to actually read a table and (eventually) use the rules
    for translation in a test

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,27 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, fs};
+
+use parser::Line;
 
 pub mod parser;
 
 #[derive(Debug, PartialEq)]
 pub struct TranslationError;
 
-pub fn translate(_table: PathBuf, input: &str) -> Result<String, TranslationError> {
+fn is_rule(line: &Line) -> bool {
+    match line {
+     	Line::Rule {rule: _, comment: _} => true,
+     	_ => false,
+    }
+//    *line == Line::Rule {rule: _, comment: _}
+}
+
+pub fn translate(table: PathBuf, input: &str) -> Result<String, TranslationError> {
+    let rules = match fs::read_to_string(table) {
+	Ok(rules) => rules,
+	Err(_) => return Err(TranslationError{}),
+    };
+    let (_, all_rules) = parser::table(&rules).unwrap();
+    let rules : Vec<Line> = all_rules.into_iter().filter(|line| is_rule(line)).collect();
     Ok(input.to_string())
 }
 
@@ -17,6 +33,6 @@ mod tests {
     #[test]
     fn it_works() {
 	let input = "testing123".to_string();
-        assert_eq!(translate(PathBuf::new(), &input), Ok(input));
+        assert_eq!(translate(PathBuf::from("tests/test_table.txt"), &input), Ok(input));
     }
 }
