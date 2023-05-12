db2873177684d88ac2a36718208d0d8332892aba
Christian Egli <christian@egli.cc>
2023-05-12 14:31:51 +0200

    Use thiserror for simplified error handling
    Wit thiserror you can wrap different errors into one error enum from
    your api.

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,27 @@
-use std::{path::PathBuf, fs};
+use std::{path::PathBuf, fs, io};
+use thiserror::Error;
 
 use parser::{Line, Rule};
 
 pub mod parser;
 
-#[derive(Debug, PartialEq)]
-pub struct TranslationError;
-
+#[derive(Error, Debug)]
+pub enum TranslationError {
+    #[error("Cannot open table")]
+    ReadTable(#[from] io::Error),
+    #[error("unknown translation error")]
+    Unknown,
+}
 pub fn translate(table: PathBuf, input: &str) -> Result<String, TranslationError> {
-    let rules = match fs::read_to_string(table) {
-	Ok(rules) => rules,
-	Err(_) => return Err(TranslationError{}),
-    };
+    let rules = fs::read_to_string(table)?;
+
     let (_, lines) = parser::table(&rules).unwrap();
     let rules : Vec<Rule> = lines.into_iter().filter_map(|line| line.as_rule()).collect();
     Ok(input.to_string())
 }
 
 pub fn debug(table: PathBuf) -> Result<Vec<Line>, TranslationError> {
-    let rules = match fs::read_to_string(table) {
-	Ok(rules) => rules,
-	Err(_) => return Err(TranslationError{}),
-    };
+    let rules = fs::read_to_string(table)?;
     let (_, all_rules) = parser::table(&rules).expect("Cannot parse table");
     Ok(all_rules)
 }
