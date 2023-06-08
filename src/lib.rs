20b63350d175cca8cf284bca324e2f95f0fa12b2
Christian Egli <christian.egli@sbs.ch>
2023-06-08 13:59:02 +0200

    Add some more docs

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,16 @@ use parser::{Line, Rule};
 pub mod parser;
 pub mod translator;
 
+/// An error encountered during braille translation.
 #[derive(Error, Debug)]
 pub enum TranslationError {
+    /// The table could not be read
     #[error("Cannot open table")]
     ReadTable(#[from] io::Error),
     #[error("unknown translation error")]
     Unknown,
 }
+
 pub fn translate(table: PathBuf, input: &str) -> Result<String, TranslationError> {
     let rules = fs::read_to_string(table)?;
 
