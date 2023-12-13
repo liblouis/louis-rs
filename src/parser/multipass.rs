bf53f5fdf038f0efd8f0cf70f168fd53933c322d
Christian Egli <christian.egli@sbs.ch>
2023-12-13 21:24:38 +0100

    Simplify the loop exit condition even more

--- src/parser/multipass.rs
+++ src/parser/multipass.rs
@@ -69,8 +69,7 @@ impl<'a> TestParser<'a> {
 
     fn ascii_number(&mut self) -> Result<u8, ParseError> {
 	let mut s = String::new();
-	while let Some(c) = self.chars.peek() {
-	    if !c.is_ascii_digit() {break;}
+	while let Some(c) = self.chars.peek().filter(|c| c.is_ascii_digit()) {
 	    s.push(self.chars.next().unwrap());
 	}
 	let number = s.parse::<u8>()?;
