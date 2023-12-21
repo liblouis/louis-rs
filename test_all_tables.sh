f72865fbba0acbfc98c46871e6e6733f32869899
Christian Egli <christian.egli@sbs.ch>
2023-12-21 09:42:36 +0100

    Use the new command line api when testing all tables

--- test_all_tables.sh
+++ test_all_tables.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-for file in ~/src/liblouis/tables/*.{ctb,utb,uti,tbl,cti,dis}; do LOUIS_TABLE_PATH=/home/eglic/src/liblouis/tables ./target/debug/louis-parser-handrolled $file > /dev/null; done
+for file in ~/src/liblouis/tables/*.{ctb,utb,uti,tbl,cti,dis}; do LOUIS_TABLE_PATH=/home/eglic/src/liblouis/tables ./target/debug/louis-parser-handrolled parse $file > /dev/null; done
