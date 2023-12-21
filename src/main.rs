e8df667626e6f088e54ceaee6449ef82b8018029
Christian Egli <christian.egli@sbs.ch>
2023-12-21 09:30:33 +0100

    Use clap for command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::io::BufRead;
 use std::io::Write;
 use std::path::Path;
+use std::path::PathBuf;
 use std::process::exit;
-use std::{env, io};
+use std::io;
+
+use clap::{Parser, Subcommand};
 
 mod parser;
 use parser::expand_includes;
@@ -10,6 +13,25 @@ use parser::table;
 use parser::RuleParser;
 use parser::TableError;
 use search_path::SearchPath;
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Parse and print debug information about the given table <TABLE>
+    Parse {
+        /// Braille table to parse. If no table is specified, a REPL
+        /// is opened and each line you enter is parsed.
+        table: Option<PathBuf>,
+    },
+}
+
+#[derive(Debug, Parser)] // requires `derive` feature
+#[command(name = "louis")]
+#[command(about = "A command line tool to translate to and from Braille")]
+#[command(author, version, long_about = None)] // Read from `Cargo.toml`
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
 fn print_errors(errors: Vec<TableError>) {
     for error in errors {
@@ -22,9 +44,9 @@ fn print_errors(errors: Vec<TableError>) {
     }
 }
 
-fn read_file(filename: &str) {
+fn read_file(file: &Path) {
     let search_path = &SearchPath::new_or("LOUIS_TABLE_PATH", ".");
-    match table(Path::new(filename)) {
+    match table(file) {
         Ok(rules) => match expand_includes(search_path, rules) {
             Ok(rules) => {
                 for rule in rules {
@@ -69,11 +91,12 @@ fn repl() {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Cli::parse();
 
-    let filename = &args.get(1);
-    match filename {
-        Some(filename) => read_file(filename),
-        None => repl(),
+    match args.command {
+	Commands::Parse { table } => match table {
+	    Some(table) => read_file(table.as_path()),
+	    None => repl(),
+	},
     }
 }
