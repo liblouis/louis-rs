df77a8efe708a6173f9bb43620bb835b90c6356b
Christian Egli <christian.egli@sbs.ch>
2023-12-22 14:29:47 +0100

    Expose the translation in the cli
    For that I made the repl function more generic. It accepts a function
    now that handles one line input. For translation this is even a
    closure that closes over the translation table.
    
    I'm not entirely sure if the mental load that I'm introducing here is
    really worth it. The whole thing could be implemented much simpler
    with a bit of code duplication

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ use clap::{Parser, Subcommand};
 
 mod parser;
 use parser::expand_includes;
-use parser::table;
+use parser::Rule;
 use parser::RuleParser;
 use parser::TableError;
 use search_path::SearchPath;
+
+use crate::translator::TranslationTable;
 
 mod translator;
 
@@ -23,6 +25,13 @@ enum Commands {
         /// Braille table to parse. If no table is specified, a REPL
         /// is opened and each line you enter is parsed.
         table: Option<PathBuf>,
+    },
+    /// translate <INPUT> to braille using <TABLE>
+    Translate {
+        /// Braille table to use for the translation
+        table: PathBuf,
+        /// String to translate
+        input: Option<String>,
     },
 }
 
@@ -46,48 +55,64 @@ fn print_errors(errors: Vec<TableError>) {
     }
 }
 
-fn read_file(file: &Path) {
+fn parse_file(file: &Path) -> Result<Vec<Rule>, Vec<TableError>> {
     let search_path = &SearchPath::new_or("LOUIS_TABLE_PATH", ".");
-    match table(file) {
-        Ok(rules) => match expand_includes(search_path, rules) {
-            Ok(rules) => {
-                for rule in rules {
-                    println!("{:?}", rule);
-                }
+    let rules = parser::table(file)?;
+    let rules = expand_includes(search_path, rules)?;
+    Ok(rules)
+}
+
+fn parse(file: &Path) {
+    match parse_file(file) {
+        Ok(rules) => {
+            for rule in rules {
+                println!("{:?}", rule);
             }
-            Err(errors) => {
-                print_errors(errors);
-            }
-        },
+        }
         Err(errors) => {
             print_errors(errors);
         }
     }
 }
 
-fn repl() {
+fn translate(table: &Path, input: &str) {
+    let rules = parser::table(table);
+    match rules {
+        Ok(rules) => {
+            let table = TranslationTable::compile(&rules);
+            println!("{}", table.translate(input));
+        }
+        Err(errors) => {
+            print_errors(errors);
+        }
+    }
+}
+
+// we pass a closure to the repl function so that we can use it for
+// both the parsing repl and the translation repl. In the case of the
+// parsing repl it is an empty closure but the translation repl closes
+// over the TranslationTable.
+fn repl(handler: Box<dyn Fn(String)>) {
     print!("> ");
     io::stdout().flush().unwrap();
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         match line {
-            Ok(line) => {
-                if !line.starts_with('#') && !line.is_empty() {
-                    let rule = RuleParser::new(&line).rule();
-                    match rule {
-                        Ok(rule) => {
-                            println!("{:?}", rule)
-                        }
-                        Err(e) => {
-                            eprintln!("{:?}", e);
-                        }
-                    }
-                    print!("> ");
-                    io::stdout().flush().unwrap();
-                }
-            }
+            Ok(line) => handler(line),
             _ => exit(0),
+        }
+        print!("> ");
+        io::stdout().flush().unwrap();
+    }
+}
+
+fn parse_line(line: String) {
+    if !line.starts_with('#') && !line.is_empty() {
+        let rule = RuleParser::new(&line).rule();
+        match rule {
+            Ok(rule) => println!("{:?}", rule),
+            Err(e) => eprintln!("{:?}", e),
         }
     }
 }
@@ -97,8 +122,25 @@ fn main() {
 
     match args.command {
         Commands::Parse { table } => match table {
-            Some(table) => read_file(table.as_path()),
-            None => repl(),
+            Some(table) => parse(table.as_path()),
+            None => repl(Box::new(parse_line)),
+        },
+        Commands::Translate { table, input } => match input {
+            Some(input) => translate(&table, &input),
+            None => {
+                let rules = parser::table(&table);
+                match rules {
+                    Ok(rules) => {
+                        let table = TranslationTable::compile(&rules);
+                        repl(Box::new(move |input| {
+                            println!("{}", table.translate(&input))
+                        }));
+                    }
+                    Err(errors) => {
+                        print_errors(errors);
+                    }
+                }
+            }
         },
     }
 }
