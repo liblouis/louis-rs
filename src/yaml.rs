f88f1e4141db960e2e140e1d22d8f087f303c463
Christian Egli <christian.egli@sbs.ch>
2024-01-11 17:39:38 +0100

    Integrate the YAML parsing with the execution of the tests
    i.e. the YAML tests are now actually executed

--- src/yaml.rs
+++ src/yaml.rs
@@ -2,8 +2,12 @@ use std::{collections::HashMap, fs::File, iter::Peekable, num::ParseIntError, pa
 
 use enumset::{EnumSet, EnumSetType};
 use libyaml::{Encoding, Event, Parser, ParserIter};
+use search_path::SearchPath;
 
-use crate::parser::Direction;
+use crate::{
+    parser::{self, expand_includes, Direction},
+    translator::TranslationTable,
+};
 
 type YAMLEventError = Option<Result<Event, libyaml::ParserError>>;
 
@@ -47,7 +51,7 @@ pub enum ParseError {
     InvalidToken(String),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 enum TestMode {
     Forward,
     Backward,
@@ -57,7 +61,7 @@ enum TestMode {
     HyphenateBraille,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 enum Table {
     Simple(PathBuf),
     Query(TableQuery),
@@ -67,12 +71,102 @@ enum Table {
 
 type TableQuery = HashMap<String, String>;
 
+#[derive(PartialEq, Debug)]
+pub enum TestResult {
+    Success,
+    Failure {
+        input: String,
+        expected: String,
+        actual: String,
+    },
+    ExpectedFailure {
+        input: String,
+        expected: String,
+        actual: String,
+    },
+    UnexpectedSuccess {
+        input: String,
+    },
+    Error,
+    NotImplemented,
+}
+
+impl TestResult {
+    pub fn is_success(&self) -> bool {
+        matches!(self, TestResult::Success { .. })
+    }
+    pub fn is_failure(&self) -> bool {
+        matches!(self, TestResult::Failure { .. })
+    }
+    pub fn is_expected_failure(&self) -> bool {
+        matches!(self, TestResult::ExpectedFailure { .. })
+    }
+    pub fn is_unexpected_success(&self) -> bool {
+        matches!(self, TestResult::UnexpectedSuccess { .. })
+    }
+}
+
+struct TestMatrix<'a> {
+    paths: Vec<&'a PathBuf>,
+    directions: &'a Vec<Direction>,
+    tests: &'a Vec<Test>,
+}
+
+impl<'a> TestMatrix<'a> {
+    fn check(&self) -> Vec<TestResult> {
+        let search_path = &SearchPath::new_or("LOUIS_TABLE_PATH", ".");
+        let mut results = Vec::new();
+        for path in &self.paths {
+            if let Some(path) = search_path.find_file(path) {
+                for direction in self.directions {
+                    if let Ok(rules) = parser::table(path.as_path()) {
+                        if let Ok(rules) = expand_includes(search_path, rules) {
+                            let table = TranslationTable::compile(rules, *direction);
+                            for test in self.tests {
+                                results.push(test.check(&table, *direction));
+                            }
+                        } else {
+                            results.push(TestResult::Error);
+                        }
+                    } else {
+                        results.push(TestResult::Error);
+                    }
+                }
+            } else {
+                results.push(TestResult::Error);
+            }
+        }
+        results
+    }
+}
+
 #[derive(Debug)]
-pub struct TestSuite {
-    display_table: Option<PathBuf>,
-    table: Table,
-    mode: TestMode,
-    tests: Vec<Test>,
+pub struct TestSuite<'a> {
+    display_table: &'a Option<PathBuf>,
+    table: &'a Table,
+    mode: &'a TestMode,
+    tests: &'a Vec<Test>,
+}
+
+impl<'a> TestSuite<'a> {
+    pub fn check(&self) -> Vec<TestResult> {
+        let directions = match self.mode {
+            TestMode::Forward => vec![Direction::Forward],
+            TestMode::Backward => vec![Direction::Backward],
+            TestMode::BothDirections => vec![Direction::Forward, Direction::Backward],
+            _ => vec![],
+        };
+        let paths = match &self.table {
+            Table::Simple(path) => vec![path],
+            _ => return vec![TestResult::NotImplemented],
+        };
+        let matrix = TestMatrix {
+            paths,
+            directions: &directions,
+            tests: &self.tests,
+        };
+        matrix.check()
+    }
 }
 
 #[derive(EnumSetType, Debug)]
@@ -88,7 +182,7 @@ pub enum TranslationMode {
 
 type Typeform = HashMap<String, String>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 enum CursorPosition {
     Single(u16),
     Tuple(u16, u16),
@@ -96,14 +190,24 @@ enum CursorPosition {
 
 type Directions = EnumSet<Direction>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 enum ExpectedFailure {
     Simple(bool),
     Reason(String),
     Direction(Directions),
 }
 
-#[derive(Debug, Clone)]
+impl ExpectedFailure {
+    fn is_failure(&self, direction: Direction) -> bool {
+        match &self {
+            Self::Simple(v) => !v,
+            Self::Reason(_) => true,
+            Self::Direction(d) => d.contains(direction),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Test {
     input: String,
     expected: String,
@@ -115,6 +219,33 @@ pub struct Test {
     modes: EnumSet<TranslationMode>,
     max_output_length: Option<u16>,
     real_input_length: Option<u16>,
+}
+
+impl Test {
+    fn check(&self, table: &TranslationTable, direction: Direction) -> TestResult {
+        let translated = table.translate(&self.input);
+        if translated == self.expected {
+            if !self.xfail.is_failure(direction) {
+                TestResult::Success
+            } else {
+                TestResult::UnexpectedSuccess {
+                    input: self.input.to_string(),
+                }
+            }
+        } else if self.xfail.is_failure(direction) {
+            TestResult::ExpectedFailure {
+                input: self.input.to_string(),
+                expected: self.expected.to_string(),
+                actual: translated,
+            }
+        } else {
+            TestResult::Failure {
+                input: self.input.to_string(),
+                expected: self.expected.to_string(),
+                actual: translated,
+            }
+        }
+    }
 }
 
 pub struct YAMLParser<'a> {
@@ -472,8 +603,8 @@ impl<'a> YAMLParser<'a> {
         }
     }
 
-    pub fn yaml(&mut self) -> Result<Vec<TestSuite>, ParseError> {
-        let mut test_suites: Vec<TestSuite> = Vec::new();
+    pub fn yaml(&mut self) -> Result<Vec<TestResult>, ParseError> {
+        let mut results = Vec::new();
         let mut current_display_table = None;
         let mut current_tables = Vec::new();
         let mut test_mode: TestMode = TestMode::Forward;
@@ -495,14 +626,14 @@ impl<'a> YAMLParser<'a> {
                 }
                 "tests" => {
                     let tests = self.tests()?;
-                    for table in current_tables.clone() {
+                    for table in &current_tables {
                         let suite = TestSuite {
-                            display_table: current_display_table.clone(),
+                            display_table: &current_display_table,
                             table: table,
-                            mode: test_mode.clone(),
-                            tests: tests.clone(),
+                            mode: &test_mode,
+                            tests: &tests,
                         };
-                        test_suites.push(suite);
+                        results.extend(suite.check());
                     }
                     current_tables.clear();
                 }
@@ -514,6 +645,6 @@ impl<'a> YAMLParser<'a> {
         self.mapping_end()?;
         self.document_end()?;
         self.stream_end()?;
-        Ok(test_suites)
+        Ok(results)
     }
 }
