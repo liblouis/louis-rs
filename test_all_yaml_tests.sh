fe0051231e9f633f7a8548e5d517a3c682cad80c
Christian Egli <christian.egli@sbs.ch>
2024-01-23 10:51:46 +0100

    The functionality to test all yaml files is integrated
    in the executable. No need to a separate shell script

--- test_all_yaml_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-for file in ~/src/liblouis/tests/yaml/*.yaml; do LOUIS_TABLE_PATH=~/src/liblouis/tables ./target/debug/louis-parser-handrolled check $file > /dev/null || echo $file; done
-for file in ~/src/liblouis/tests/braille-specs/*.yaml; do LOUIS_TABLE_PATH=~/src/liblouis/tables ./target/debug/louis-parser-handrolled check $file > /dev/null || echo $file; done
