6b44bc31a2ff2e866020456793a6fa759f57127a
Christian Egli <christian.egli@sbs.ch>
2024-01-25 10:51:27 +0100

    Drop the dependency on regex

--- src/metadata.rs
+++ src/metadata.rs
@@ -7,12 +7,9 @@ use std::{
 #[derive(thiserror::Error, Debug)]
 pub enum MetaDataError {
     #[error(transparent)]
-    RegexError(#[from] regex::Error),
-    #[error(transparent)]
     IoError(#[from] std::io::Error),
 }
 
-use regex::Regex;
 use search_path::SearchPath;
 
 type Query = Vec<(String, String)>;
@@ -44,11 +41,23 @@ pub fn index() -> Result<Index, MetaDataError> {
             let path = entry?.path();
 
             if let Ok(content) = fs::read_to_string(&path) {
-                let re = Regex::new(r"(?m)^#\+([^:]+):(.+)$")?;
-                for (_, [key, value]) in re.captures_iter(&content).map(|c| c.extract()) {
-                    let k = key.trim().into();
-                    let v = value.trim().into();
-                    index.entry((k, v)).or_default().insert(path.clone());
+                for line in content.lines() {
+                    if let Some(line) = line.strip_prefix("#+") {
+                        if let Some((key, value)) = line.split_once(":") {
+                            let k = key.trim();
+                            let v = value.trim();
+                            if !k.is_empty()
+                                && !v.is_empty()
+                                && k.chars().all(|c| c.is_ascii_alphanumeric())
+                                && v.chars().all(|c| c.is_ascii_graphic())
+                            {
+                                index
+                                    .entry((k.into(), v.into()))
+                                    .or_default()
+                                    .insert(path.clone());
+                            }
+                        }
+                    }
                 }
             }
         }
